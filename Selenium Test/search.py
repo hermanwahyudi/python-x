389470c46a5e431765f23ca57ffba1e452a95491
Herman TKPD <herman.wahyudi@tokopedia.com>
2014-12-01 14:52:53 +0700

    Add folder Selenium Python

---
+++ Selenium Test/search.py
@@ -0,0 +1,17 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+
+class SearchPage(unittest.TestCase):
+	def __init__(self):
+		return None
+
+	def setUp(self):
+
+	def __str__(self):
+		return "Class Search"
+
+# main
+if(__name__ == "__main__"):
+	unittest.main()
