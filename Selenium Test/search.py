08433b4cbd1997fbb7a0b3db054aad3e0f7ddf3b
Herman <herman.wahyudi02@gmail.com>
2014-12-04 23:14:37 +0700

    Push search.py

--- Selenium Test/search.py
+++ Selenium Test/search.py
@@ -1,36 +1,19 @@
 from selenium import webdriver
+from random import randint
 
 class Search:
 	def __init__(self):
-<<<<<<< HEAD
 		self.browser = webdriver.Firefox()
 		self.browser.get("https://tokopedia.com/")
 	
 	def searchTkpd(self, sesuatu):
-		self.browser.find_element_by_name("search_keyword").send_keys(sesuatu)
-		self.browser.find_element_by_css_selector("button.btn-search").click()
-		self.browser.find_element_by_id("tab-shop").click()
-=======
-		return None
-
-	def setUp(self):
-		self.driver = webdriver.Firefox() 
-
-	def search(self):
-		driver = self.driver
-		driver.get("https://tokopedia.com/")
-		elem = driver.find_element_by_name("search_keyword")
-		elem.send_keys("Nokia")
-		assert "No result found." not in driver.page_source
-		elem.send_keys(Keys.RETUNR)
-
-	def tearDown(self):
-		self.driver.close()
->>>>>>> b_herman
-
-
+		for i in range(100):
+			self.browser.find_element_by_name("search_keyword").send_keys(sesuatu)
+			self.browser.find_element_by_css_selector("button.btn-search").click()
+			self.browser.find_element_by_id("tab-shop").click()
+			self.browser.get("https://tokopedia.com/")
 # main
 
 if(__name__ == "__main__"):
 	obj = Search()
-	obj.searchTkpd("Toko QC 14")
+	obj.searchTkpd(randint(10000000, 100000000))
