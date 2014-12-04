637f180dff01db143a8ace23bb1855f6481ba983
Herman <herman.wahyudi02@gmail.com>
2014-12-04 23:38:46 +0700

    Push search unittest

--- Selenium Test/searchUnitTest_PageObject.py
+++ Selenium Test/searchUnitTest_PageObject.py
@@ -4,19 +4,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class SearchPage(unittest.TestCase):
-	def __init__(self):
-		return None
-
 	def setUp(self):
 		self.driver = webdriver.Firefox() 
 
-	def search(self):
+	def test_search(self):
 		driver = self.driver
 		driver.get("https://tokopedia.com/")
 		elem = driver.find_element_by_name("search_keyword")
 		elem.send_keys("Nokia")
 		assert "No result found." not in driver.page_source
-		elem.send_keys(Keys.RETUNR)
+		elem.send_keys(Keys.RETURN)
 
 
 	def tearDown(self):
